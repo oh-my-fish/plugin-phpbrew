a310f4d87f04901ccc99f20feb567c7e6f6c9a21
Stephen Coakley <me@stephencoakley.com>
2016-03-24 14:26:29 -0500

    Smarter path handling

--- functions/__phpbrew_update_path.fish
+++ functions/__phpbrew_update_path.fish
@@ -1,8 +1,13 @@
 # Puts the current PHP version into the path.
 function __phpbrew_update_path
-  if not set -q PATH_WITHOUT_PHPBREW
-    set -gx PATH_WITHOUT_PHPBREW $PATH
-  end
+  # Strip old phpbrew paths from the path.
+  set -gx PATH (for p in $PATH
+    echo "$p"
+  end | grep -v "^$PHPBREW_ROOT")
 
-  set -gx PATH $PHPBREW_PATH $PHPBREW_BIN $PATH_WITHOUT_PHPBREW
+  # Add the current PHP version paths to the path if set.
+  set -q PHPBREW_BIN
+    and set -gx PATH $PHPBREW_BIN $PATH
+  set -q PHPBREW_PATH
+    and set -gx PATH $PHPBREW_PATH $PATH
 end
